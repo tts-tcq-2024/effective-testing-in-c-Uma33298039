4fb33a22c82944217f41727631b5c603f10a26fe
Uma33298039 <umamageshwari098@gmail.com>
2024-08-11 08:45:44 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -5,9 +5,9 @@ int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
+     // Return 200 for ok
+     // Return 500 for not-ok
+     // stub always succeeds and returns 200
     return 500;
 }
 
