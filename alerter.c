3c480364b136afa7326d1c4906f4e3d92302237f
Uma33298039 <umamageshwari098@gmail.com>
2024-08-19 10:57:50 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -11,13 +11,18 @@ int alertFailureCount = 0;
 //    return 0;
 //}
 
-void alertInCelcius(float farenheit, int(*networkAlerter)(float)) {
+void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlerter(celcius);
+    int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
 }
+
 int networkAlertStub(float celcius) {
     if ((celcius <NORMAL_TEMP_MIN)|| (celcius <NORMAL_TEMP_MAX))
     {
