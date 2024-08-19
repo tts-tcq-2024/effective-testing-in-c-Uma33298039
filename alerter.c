e1e2f5436a3159bbe919688395197b970297610f
Uma33298039 <umamageshwari098@gmail.com>
2024-08-19 11:00:25 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define NORMAL_TEMP_MIN 150.0
-#define NORMAL_TEMP_MAX 200.0
-
 int alertFailureCount = 0;
 
-//int realTimenetworkAleter(float celcius)
-//{
-//    return 0;
-//}
+int networkAlertStub(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+     // Return 200 for ok
+     // Return 500 for not-ok
+     // stub always succeeds and returns 200
+    return 500;
+}
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
@@ -23,38 +23,11 @@ void alertInCelcius(float farenheit) {
     }
 }
 
-int networkAlertStub(float celcius) {
-    if ((celcius <NORMAL_TEMP_MIN)|| (celcius <NORMAL_TEMP_MAX))
-    {
-        return 200;
-    }
-    else
-    {
-        return 500;
-    }  
-}
-float actualCelicusReceived;
-int networkAlertCallCount = 0;
-
-int networkAlertMock(float celcius) {
-    actualCelicusReceived =celcius;
-    ++networkAlertCallCount;
-    return 500;
-}
-void stateBasedtest()
-{
-    alertInCelcius(400.5,&networkAlertStub);
-    assert(alertFailureCount ==1);
-}
-void behaviorTest(){
-    alertInCelcius(303.6,&networkAlertMock);
-    assert(alertFailureCount ==1);
-    float expectedCelToBeReceivdByDepedency = 204.72222;
-    assert(actualCelicusReceived ==expectedCelToBeReceivdByDepedency); 
-    assert(networkAlertCallCount ==1); 
-}
 
 int main() {
+    alertInCelcius(400.5);
+    alertInCelcius(303.6);
+    assert(alertFailureCount == 2); 
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
